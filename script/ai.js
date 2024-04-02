450880689339b45d5fb090eb60d813025630970a
Métöushela walker <160696674+Metoushael17@users.noreply.github.com>
2024-04-02 18:05:29 +0200

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,33 +1,29 @@
 const axios = require('axios');
+
 module.exports.config = {
-  name: 'ai',
-  version: '1.0.0',
-  role: 0,
-  hasPrefix: false,
-  aliases: ['gpt', 'openai'],
-  description: "An AI command powered by GPT-4",
-  usage: "Ai [promot]",
-  credits: 'Developer',
-  cooldown: 3,
+		name: "Ai",
+		version: 1.0,
+		credits: "OtinXSandip",
+		description: "AI",
+		hasPrefix: false,
+		usages: "{pn} [prompt]",
+		aliases: ["gpt4","ai","megan","Métöushela","fataki","Megan AE"],
+		cooldown: 0,
 };
-module.exports.run = async function({
-  api,
-  event,
-  args
-}) {
-  const input = args.join(' ');
-  if (!input) {
-    api.sendMessage(`Hey I'm your virtual assistant, ask me a question.'`, event.threadID, event.messageID);
-    return;
-  }
-  api.sendMessage(`🔍 "${input}"`, event.threadID, event.messageID);
-  try {
-    const {
-      data
-    } = await axios.get(`https://openaikey-x20f.onrender.com/api?prompt=${encodeURIComponent(input)}`);
-    const response = data.response;
-    api.sendMessage(response, event.threadID, event.messageID);
-  } catch (error) {
-    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
-  }
+
+module.exports.run = async function ({ api, event, args }) {
+		try {
+				const prompt = args.join(" ");
+				if (!prompt) {
+						await api.sendMessage("Hey I'm your virtual assistant, ask me a question.", event.threadID);
+						return;
+				}
+
+				const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+				const answer = response.data.answer;
+
+				await api.sendMessage(answer, event.threadID);
+		} catch (error) {
+				console.error("Error:", error.message);
+		}
 };
